eeac6218f5a4bf0c828741a3c53b84ff26bf079f
prithvidiamond1 <prithviraj645@gmail.com>
2020-10-07 14:01:24 +0530

    Made changes to comments in Shuffler.py

--- Shuffler.py
+++ Shuffler.py
@@ -4,7 +4,7 @@ def generateShuffledPack():
     suites = ["C", "S", "H", "D"] # Generating Suites
     cards = ["A", "J", "Q", "K"]+["0"+str(_) if _ < 10 else str(_) for _ in range(2, 11)] # Generating cards per suite
     pack = [card+" - "+suite for card in cards for suite in suites] # Generating unshuffled pack
-    for card in r.sample(pack, 52): # Generating shuffled pack and displaying it
+    for card in r.sample(pack, 52): # shuffling pack and displaying it
         print(card)
 
 generateShuffledPack()
